424d2b24dce8b40611e09d8da92e7251b8a3eaa5
hadove02 <hadove02@naver.com>
2024-06-03 17:32:04 +0900

    delete all file

--- controllers/loginController.js
+++
@@ -1,2 +0,0 @@
-exports.login = (req, res) => {
-}
--- routes/login.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/loginController');
-
-router.get('/api/login', controller.login);
-
-module.exports = router;
